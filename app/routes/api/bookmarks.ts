4c3101b92d5fb1e7cfc6049aaac72636778632f3
Raulif <raul4cade@gmail.com>
2025-05-06 11:53:18 +0200

    Add put method

--- app/routes/api/bookmarks.ts
+++ app/routes/api/bookmarks.ts
@@ -5,25 +5,25 @@ import { api } from "../../../convex/_generated/api";
 import { postBookmarksToDB, getBookmarksFromDB } from "../../db/bookmarks";
 import { c } from "vinxi/dist/types/lib/logger";
 
-
 export const APIRoute = createAPIFileRoute("/api/bookmarks")({
   POST: async ({ request, params }) => {
     try {
-      const existingBookmarks = await getBookmarksFromDB();
+      const response = await getBookmarksFromDB();
+      const existingBookmarks = response.length ? response[0].bookmarks : [];
       const reqBookmarks = await request.json();
       const bookmarks = reqBookmarks.map((bookmark: any) => {
-        const existing = existingBookmarks.find(b => b.id === bookmark.id);
+        const existing = existingBookmarks.find((b) => b.id === bookmark.id);
         if (existing) {
-          return existing
+          return existing;
         } else {
           return {
             ...bookmark,
             createdAt: new Date().toISOString(),
             consumed: false,
-          }
+          };
         }
-      })
-      await postBookmarksToDB({...existingBookmarks, ...bookmarks});
+      });
+      await postBookmarksToDB({ ...existingBookmarks, ...bookmarks });
       return json(
         { message: "Bookmarks added successfully" },
         {
@@ -43,7 +43,9 @@ export const APIRoute = createAPIFileRoute("/api/bookmarks")({
 
   GET: async ({ request, params }) => {
     try {
-      const bookmarks = await getBookmarksFromDB();
+      const response = await getBookmarksFromDB();
+      const bookmarks = response.length ? response[0].bookmarks : [];
+
       return json(bookmarks, { status: 200 });
     } catch (error) {
       return json({ error: error.message }, { status: 500 });
@@ -62,5 +64,35 @@ export const APIRoute = createAPIFileRoute("/api/bookmarks")({
         },
       }
     );
+  },
+
+  PUT: async ({ request, params }) => {
+    try {
+      const bookmark = await request.json();
+      const response = await getBookmarksFromDB();
+      const bookmarks = response.length ? response[0].bookmarks : [];
+
+      const updatedBookmarks = bookmarks.map((b) => {
+        if (b.bookmarkId === bookmark.id) {
+          console.log("found bookmark", b);
+          return { ...b, consumed: bookmark.consumed };
+        }
+        return b;
+      });
+      await postBookmarksToDB(updatedBookmarks);
+      return json(
+        { message: "Bookmark updated successfully" },
+        {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+          },
+        }
+      );
+    } catch (error) {
+      return json({ error: error.message }, { status: 500 });
+    }
   },
 });
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,16 +3,15 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { api } from "../../convex/_generated/api";
 import { testData } from "../../test";
+import "../styles.css";
+import { use, useCallback, useMemo } from "react";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  const { data } = useSuspenseQuery(
-    convexQuery(api.bookmarks.get, {})
-  );
-  console.log(data);
+  const { data } = useSuspenseQuery(convexQuery(api.bookmarks.get, {}));
 
   const onClick = async () => {
     try {
@@ -29,20 +28,70 @@ function Home() {
     }
   };
 
-  const bookmarks = data.length ? data[0].bookmarks : [];
+  const bookmarks = useMemo(
+    () =>
+      data.length
+        ? data[0].bookmarks.sort((a, b) => a.dateAdded - b.dateAdded)
+        : [],
+    []
+  );
 
+  const formatDate = (date: number) =>
+    new Date(date).toLocaleString("de", {
+      year: "numeric",
+      month: "2-digit",
+      day: "numeric",
+    });
+
+  const onCheckboxChange = useCallback((event) => {
+    const input = event.target as HTMLInputElement;
+    const { checked } = input;
+    const id = input.dataset.id;
+    fetch("api/bookmarks", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, consumed: checked }),
+    });
+  }, []);
+  console.log("bookmarks", bookmarks);
   return (
-    <div>
-      <h1>Bookmarks</h1>
-      <button onClick={onClick}>Add Bookmarks</button>
-      <ul>
-        {bookmarks?.map((bookmark: any) => (
-          <li key={bookmark.id}>
-            <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
-              {bookmark.title}
-            </a>
-            <p>{bookmark.description}<p>
-            <p>{bookmark.createdAt}</p>
+    <div className="px-10 py-4">
+      <div className="flex justify-between w-full mb-12 items-center">
+        <h1 className="text-5xl">Bookmarks</h1>
+        {!bookmarks.length && (
+          <button className="border broder-1 py-1 px-2" onClick={onClick}>
+            Add Bookmarks
+          </button>
+        )}
+      </div>
+      <ul className="flex flex-col gap-2">
+        {bookmarks?.map((bookmark: any, index) => (
+          <li
+            key={bookmark.bookmarkId}
+            className="flex justify-between items-start"
+          >
+            <div className="flex gap-1 flex-col">
+              <p className="text-sm">{formatDate(bookmark.dateAdded)}</p>
+              <div className="flex gap-2">
+                <span>{index + 1}</span>
+                <a
+                  href={bookmark.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline hover:text-blue-600"
+                >
+                  {bookmark.title}
+                </a>
+              </div>
+            </div>
+            <input
+              data-id={bookmark.bookmarkId}
+              type="checkbox"
+              checked={bookmark.consumed}
+              onChange={onCheckboxChange}
+            />
           </li>
         ))}
       </ul>
