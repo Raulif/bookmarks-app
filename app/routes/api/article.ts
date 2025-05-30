fefab31ee3849e754cb89769b29721cceed2ba20
Raulif <raul4cade@gmail.com>
2025-05-30 16:14:24 +0200

    Remove new lines

--- app/routes/api/article.ts
+++ app/routes/api/article.ts
@@ -1,9 +1,9 @@
-import { Defuddle } from "defuddle/node";
-import { convert } from "html-to-text";
-import { json } from "@tanstack/react-start";
-import { setResponseStatus } from "@tanstack/react-start/server";
-import { createAPIFileRoute } from "@tanstack/react-start/api";
-import { JSDOM } from "jsdom";
+import { Defuddle } from 'defuddle/node';
+import { convert } from 'html-to-text';
+import { json } from '@tanstack/react-start';
+import { setResponseStatus } from '@tanstack/react-start/server';
+import { createAPIFileRoute } from '@tanstack/react-start/api';
+import { JSDOM } from 'jsdom';
 
 const convertOptions = {
   formatters: {
@@ -11,25 +11,28 @@ const convertOptions = {
   },
   selectors: [
     {
-      selector: "a",
+      selector: 'a',
       options: { ignoreHref: true },
     },
-    { selector: "code", format: "skipCodeTags" },
+    { selector: 'img', format: 'skip' },
+    { selector: 'code', format: 'skipCodeTags' },
   ],
 };
 
-export const APIRoute = createAPIFileRoute("/api/article")({
+export const APIRoute = createAPIFileRoute('/api/article')({
   GET: async ({ request, params }) => {
     try {
       const requestUrl = new URL(request.url);
-      const targetUrl = new URL(requestUrl.searchParams.get("url") || "");
+      const targetUrl = new URL(requestUrl.searchParams.get('url') || '');
       // Get document HTML
       const dom = await JSDOM.fromURL(targetUrl.toString());
       // Clean up HTML
       const { content, title } = await Defuddle(dom);
       // Get content as readable text
       const text = convert(content, convertOptions);
-      const textWithTitle = `Title: ${title}. Article: ${text}.`;
+      const parsedText = text.replace(/\n/g, ' ')
+      const textWithTitle = `Title: ${title}. Article: ${parsedText}.`;
+      console.log(textWithTitle)
       setResponseStatus(200);
       return json(textWithTitle);
     } catch (error) {
